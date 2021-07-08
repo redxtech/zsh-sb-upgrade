09dc9bd33e63df18712324738cd5980aed7e9347
Gabe Dunn <gdunn@pipewise.ca>
2021-07-08 15:02:38 -0600

    add colours

--- zsh-sb-upgrade.plugin.zsh
+++ zsh-sb-upgrade.plugin.zsh
@@ -2,6 +2,10 @@
 
 # zsh plugin to upgrade apps on my seedbox
 
+# load colors
+autoload -Uz colors
+colors
+
 # add the functions to the function path & autoload them
 fpath+=("$(dirname ${0})/functions")
 autoload -Uz sb-upgrade
